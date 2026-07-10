4d489bab1d26128d2e33a3ea5f89cc09ff510ece
Hammam <hemo33272@gmail.com>
2026-07-10 18:36:42 +0300

    fix scroll position

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "../common/Header";
 import Footer from "../common/Footer";
+import ScrollToTop from "../common/ScrollToTop";
 
 function Layout() {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
