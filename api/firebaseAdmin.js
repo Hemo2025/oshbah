6366360c50883c4801669b9ba9311ec79f37c8bc
Hammam <hemo33272@gmail.com>
2026-07-22 11:42:51 +0300

    debug firebase admin

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -1,11 +1,21 @@
 import admin from "firebase-admin";
 
+console.log("PROJECT ID:", process.env.FIREBASE_PROJECT_ID);
+console.log(
+  "CLIENT EMAIL:",
+  process.env.FIREBASE_CLIENT_EMAIL ? "EXISTS" : "MISSING",
+);
+console.log(
+  "PRIVATE KEY:",
+  process.env.FIREBASE_PRIVATE_KEY ? "EXISTS" : "MISSING",
+);
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
   });
 }
