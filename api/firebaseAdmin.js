163ed7414dde8956a6b445e23c08e670b6055c3a
Hammam <hemo33272@gmail.com>
2026-07-22 11:49:16 +0300

    fix firebase admin credential

--- api/firebaseAdmin.js
+++ api/firebaseAdmin.js
@@ -1,22 +1,14 @@
 import admin from "firebase-admin";
 
-console.log("PROJECT ID:", process.env.FIREBASE_PROJECT_ID);
-console.log(
-  "CLIENT EMAIL:",
-  process.env.FIREBASE_CLIENT_EMAIL ? "EXISTS" : "MISSING",
-);
-console.log(
-  "PRIVATE KEY:",
-  process.env.FIREBASE_PRIVATE_KEY ? "EXISTS" : "MISSING",
-);
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
