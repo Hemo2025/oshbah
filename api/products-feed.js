0f3063c38187a0df49e0d2a8e1b86d00e61b12b1
Hammam <hemo33272@gmail.com>
2026-07-28 08:03:22 +0300

    dsadaf

--- api/products-feed.js
+++ api/products-feed.js
@@ -19,12 +19,25 @@ xmlns:g="http://base.google.com/ns/1.0">
       const title = product.name || "";
       const description = product.description || "";
       const slug = product.slug || doc.id;
+
       const image = product.images?.[0] || "";
+      const extraImages =
+        product.images
+          ?.slice(1)
+          .map(
+            (img) =>
+              `<g:additional_image_link>${img}</g:additional_image_link>`,
+          )
+          .join("\n") || "";
+
       const price = Number(product.price || 0).toFixed(2);
+      const oldPrice = Number(product.oldPrice || 0).toFixed(2);
+
       const availability = product.stock > 0 ? "in stock" : "out of stock";
 
       xml += `
 <item>
+
 <g:id>${doc.id}</g:id>
 
 <g:title><![CDATA[${title}]]></g:title>
@@ -35,19 +48,47 @@ xmlns:g="http://base.google.com/ns/1.0">
 
 <g:image_link>${image}</g:image_link>
 
+${extraImages}
+
 <g:availability>${availability}</g:availability>
 
 <g:condition>new</g:condition>
 
 <g:price>${price} SAR</g:price>
 
-<g:brand>Oshbah Store</g:brand>
+${
+  product.oldPrice && product.oldPrice > product.price
+    ? `<g:sale_price>${price} SAR</g:sale_price>
+<g:price>${oldPrice} SAR</g:price>`
+    : ""
+}
+
+<g:brand>Oshbah</g:brand>
 
 <g:identifier_exists>false</g:identifier_exists>
 
-<g:product_type><![CDATA[${product.category || "General"}]]></g:product_type>
+<g:product_type><![CDATA[${
+        product.category || "Health & Beauty"
+      }]]></g:product_type>
 
-<g:google_product_category>Health &amp; Beauty</g:google_product_category>
+<g:google_product_category>
+Health &amp; Beauty &gt; Health Care
+</g:google_product_category>
+
+<g:item_group_id>${doc.id}</g:item_group_id>
+
+<g:shipping>
+<g:country>SA</g:country>
+<g:service>Standard</g:service>
+<g:price>0 SAR</g:price>
+</g:shipping>
+
+<g:adult>no</g:adult>
+
+<g:age_group>adult</g:age_group>
+
+<g:gender>unisex</g:gender>
+
 </item>
 `;
     });
