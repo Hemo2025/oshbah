a218b2bcc55a53dacfef7428bb1dc18d824daaa1
Hammam <hemo33272@gmail.com>
2026-08-03 23:23:58 +0300

    sadfasf

--- api/products-feed.js
+++ api/products-feed.js
@@ -1,10 +1,10 @@
-  import { adminDb } from "./firebaseAdmin.js";
+import { adminDb } from "./firebaseAdmin.js";
 
-  export default async function handler(req, res) {
-    try {
-      const snapshot = await adminDb.collection("products").get();
+export default async function handler(req, res) {
+  try {
+    const snapshot = await adminDb.collection("products").get();
 
-      let xml = `<?xml version="1.0" encoding="UTF-8"?>
+    let xml = `<?xml version="1.0" encoding="UTF-8"?>
   <rss version="2.0"
   xmlns:g="http://base.google.com/ns/1.0">
   <channel>
@@ -13,29 +13,29 @@
   <description>Oshbah Store Product Feed</description>
   `;
 
-      snapshot.forEach((doc) => {
-        const product = doc.data();
+    snapshot.forEach((doc) => {
+      const product = doc.data();
 
-        const title = product.name || "";
-        const description = product.description || "";
-        const slug = product.slug || doc.id;
+      const title = product.name || "";
+      const description = product.description || "";
+      const slug = product.slug || doc.id;
 
-        const image = product.images?.[0] || "";
-        const extraImages =
-          product.images
-            ?.slice(1)
-            .map(
-              (img) =>
-                `<g:additional_image_link>${img}</g:additional_image_link>`,
-            )
-            .join("\n") || "";
+      const image = product.images?.[0] || "";
+      const extraImages =
+        product.images
+          ?.slice(1)
+          .map(
+            (img) =>
+              `<g:additional_image_link>${img}</g:additional_image_link>`,
+          )
+          .join("\n") || "";
 
-        const price = Number(product.price || 0).toFixed(2);
-        const oldPrice = Number(product.oldPrice || 0).toFixed(2);
+      const price = Number(product.price || 0).toFixed(2);
+      const oldPrice = Number(product.oldPrice || 0).toFixed(2);
 
-        const availability = product.stock > 0 ? "in stock" : "out of stock";
+      const availability = product.stock > 0 ? "in stock" : "out of stock";
 
-        xml += `
+      xml += `
   <item>
 
   <g:id>${doc.id}</g:id>
@@ -54,13 +54,15 @@
 
   <g:condition>new</g:condition>
 
-  <g:price>${price} SAR</g:price>
-
   ${
-    product.oldPrice && product.oldPrice > product.price
-      ? `<g:sale_price>${price} SAR</g:sale_price>
-  <g:price>${oldPrice} SAR</g:price>`
-      : ""
+    product.oldPrice && Number(product.oldPrice) > Number(product.price)
+      ? `
+<g:price>${oldPrice} SAR</g:price>
+<g:sale_price>${price} SAR</g:sale_price>
+`
+      : `
+<g:price>${price} SAR</g:price>
+`
   }
 
   <g:brand>Oshbah</g:brand>
@@ -68,8 +70,8 @@
   <g:identifier_exists>false</g:identifier_exists>
 
   <g:product_type><![CDATA[${
-          product.category || "Health & Beauty"
-        }]]></g:product_type>
+    product.category || "Health & Beauty"
+  }]]></g:product_type>
 
   <g:google_product_category>
   Health &amp; Beauty &gt; Health Care
@@ -80,7 +82,7 @@
   <g:shipping>
   <g:country>SA</g:country>
   <g:service>Standard</g:service>
-  <g:price>0 SAR</g:price>
+  <g:price>0.00 SAR</g:price>
   </g:shipping>
 
   <g:adult>no</g:adult>
@@ -91,16 +93,16 @@
 
   </item>
   `;
-      });
+    });
 
-      xml += `
+    xml += `
   </channel>
   </rss>`;
 
-      res.setHeader("Content-Type", "application/xml; charset=utf-8");
-      res.status(200).send(xml);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Feed Error");
-    }
+    res.setHeader("Content-Type", "application/xml; charset=utf-8");
+    res.status(200).send(xml);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Feed Error");
   }
+}
